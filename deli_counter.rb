72f82ff08736a5728598e62b3e8deb299ad40fa6
Sean Lucius <seanlucius22@icloud.com>
2019-01-17 04:32:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,9 @@ def line(deli_line)
   if deli_line.empty?
     puts "The line is currently empty."
   else 
+    line_list = deli_line.each do |spot|
+      
+    end 
     puts "The line is currently "
   end 
 end 

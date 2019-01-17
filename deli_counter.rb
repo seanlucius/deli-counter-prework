b673bd7d2535bccaeaf1930c12910cbb8579fb68
Sean Lucius <seanlucius22@icloud.com>
2019-01-17 04:51:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(deli_line)
   if deli_line.empty?
     puts "The line is currently empty."
   else 
-    deli_line.map do |spot|
+    line_list = deli_line.map do |spot|
       "#{deli_line.index(spot) + 1}. #{spot}"
     end 
-    puts "The line is currently: #{deli_line.join(" ")}"
+    puts "The line is currently: #{line_list.join(" ")}"
   end 
 end 
